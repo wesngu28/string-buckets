e77fbc0bb0782baf448aaea9173e444c2f3d4bac
wesngu28 <wesley@wesngu28.com>
2023-11-07 21:37:07 -0800

    Add drag handlers and containers

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,49 @@ Amon-Ra St Brown
 Gardner Minshew`
 
 export default function Home() {
-
   const [items, setItems] = useState(strings.split('\n'))
+  const [categories, setCategories] = useState<Array<{name: string, items: string[]}>>([])
+
+  function handleOnDrag(e: React.DragEvent, text: string) {
+    e.dataTransfer.setData("text", text)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+      <div className="flex flex-col items-center">
+        <div className="flex gap-8 flex-wrap m-8">
+          {categories.map(category => 
+            <div onDrop={(e) => {
+              setItems(items.filter(item => item !== e.dataTransfer.getData("text")))
+              const updatedCategories = categories.map((c) => {
+                if (c.name === category.name) {
+                  return {
+                    ...c,
+                    items: [...c.items, e.dataTransfer.getData("text")],
+                  };
+                }
+                return c
+              }) as [];
+              setCategories(updatedCategories);
+            }} onDragOver={(e) => {
+              e.preventDefault()
+              console.log(e)
+            }} className="bg-gray-400 rounded-lg p-4 text-gray-800 min-h-[18rem] min-w-[18rem] text-center">
+              <p className="text-2xl">{category.name}</p>
+              {category.items.map(item => <p>{item}</p>)}
+            </div>
+          )}
+        </div>
+      </div>
       <div className="flex gap-4 flex-wrap">
-        {items.map(string => <p key={string}>{string}</p>)}
+          {items.map(string => 
+          <p 
+            draggable={true} 
+            key={string}
+            onDragStart={(e) => handleOnDrag(e, string)}
+          >
+            {string}
+          </p>)}
       </div>
     </main>
   )
